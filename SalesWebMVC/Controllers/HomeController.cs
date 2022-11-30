d51bd9a773d4f33699b69c899463d2783fc94a4a
Wendel Florencio <wendel.github@gmail.com>
2022-11-30 15:26:08 -0300

    First controller and Razor pages tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["email"] = "wendelreis@gmail.com";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
